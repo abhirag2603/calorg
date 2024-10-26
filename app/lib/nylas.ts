c2f836bfd9fcdfd96da343b0567df28e2ca2c579
Abhirag <abhirag4169@gmail.com>
2024-10-26 23:40:52 +0530

    implemented nylas authentication

--- app/lib/nylas.ts
+++ app/lib/nylas.ts
@@ -8,8 +8,8 @@ apiUri:process.env.NYLAS_API_URI!,
 
 
 export const nylasConfig={
-    clientId:"",
-    redirectUri:"",
+    clientId:process.env.NYLAS_CLIENT_ID!,
+    redirectUri:process.env.NEXT_PUBLIC_URL+"/api/oauth/exchange",
     apiKey:process.env.NYLAS_API_KEY!,
     apiUri:process.env.NYLAS_API_URI!
 }
