3f8ea1b45ea64a9213e97777616d61d2a2b170dd
Abhirag <abhirag4169@gmail.com>
2025-02-16 21:34:04 +0530

    title/logo fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <link rel="icon" href="/logo.png  " sizes="any" />
+      <link rel="icon" href="/logo.png" sizes="any" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
