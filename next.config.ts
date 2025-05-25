af0be7198014aa3e4ef2c9c66287b4d166f2573f
Abhirag <abhirag4169@gmail.com>
2025-05-25 20:16:48 +0530

    github auth config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["utfs.io"], // Add the hostname here
+    domains: ["utfs.io","avatars.githubusercontent.com"], 
   },
 
 };
